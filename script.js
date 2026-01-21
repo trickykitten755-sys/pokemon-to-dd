7c5597216548fc034967c52a6eb50efff501e526
trickykitten755-sys <trickykitten755@gmail.com>
2026-01-21 12:04:55 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+const STAT_TO_ABILITY = [
+  [1, 40, 8],
+  [41, 60, 10],
+  [61, 80, 12],
+  [81, 100, 14],
+  [101, 120, 16],
+  [121, 140, 18],
+  [141, 160, 20],
+  [161, 220, 22],
+  [221, 999, 25]
+];
+
+const TYPE_MAP = {
+  normal: "Bludgeoning/Slashing",
+  fire: "Fire",
+  water: "Cold",
+  electric: "Lightning",
+  grass: "Poison/Slashing",
+  ice: "Cold",
+  fighting: "Bludgeoning",
+  poison: "Poison",
+  ground: "Bludgeoning",
+  flying: "Slashing",
+  psychic: "Psychic",
+  bug: "Piercing",
+  rock: "Bludgeoning",
+  ghost: "Necrotic",
+  dragon: "Force",
+  dark: "Necrotic",
+  steel: "Slashing",
+  fairy: "Radiant"
+};
